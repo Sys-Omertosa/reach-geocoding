de3ff91ae8b87eb5375303509bc7047e609db707
= <=>
2025-12-13 10:55:47 +0500

    Working but unclean markdown function

--- Backend/processor/llm/llm_client.py
+++ Backend/processor/llm/llm_client.py
@@ -1,10 +1,39 @@
 import os
-import json
 from openai import OpenAI
-from utils import load_dotenv
+from utils import load_env
 
-configs = json.load("llm_configs.json")
-load_dotenv()
+load_env()
+configs = {
+    "ernie-4.5-vl": {
+        "model": "ernie-4.5-vl-28b-a3b-thinking",
+        "api_key_name": "BAIDU_KEY",
+        "base_url": "https://aistudio.baidu.com/llm/lmapi/v3",
+        "default_params": {
+            "extra_body": {
+                "penalty_score": 1
+            },
+            "max_completion_tokens": 128000,
+            "temperature": 0.6,
+            "top_p": 0.95,
+            "frequency_penalty": 0,
+            "presence_penalty": 0
+        }
+    },
+    "ernie-x1": {
+        "model": "ernie-x1.1-preview",
+        "api_key_name": "BAIDU_KEY",
+        "base_url": "https://aistudio.baidu.com/llm/lmapi/v3",
+        "default_params": {
+            "web_search": {
+                "enable": "True"
+            },
+            "max_completion_tokens": 65536,
+            "response_format": {
+                "type": "json_object"
+            }
+        }
+    }
+}
 
 # Unified LLM client with abstraction, based on Openai
 class LLMClient:
@@ -17,20 +46,25 @@ class LLMClient:
         self._client = self._create_client()
 
     def _create_client(self) -> OpenAI:
-        # Create an OpenAI client with the configured key and base_url
+        """Create an OpenAI client with the configured key and base_url"""
         key = os.getenv(self.config.get("api_key_name"))
+        if not key:
+            raise ValueError(f"API key not found for {self.config.get('api_key_name')}")
+        
         url = self.config.get("base_url")
 
         return OpenAI(api_key=key, base_url=url)
     
     def call(self, messages, **kwargs):
-        # Make a call to the LLM
-
+        """Make a call to the LLM"""
+        #call(self, messages, **kwargs)
         #Merge default and custom params
         params = {**self.config["default_params"], **kwargs}
+        #params = self.config["default_params"]
         
-        return self._client.chat.completions.create(
-            model = self.model,
+        response = self._client.chat.completions.create(
+            model = self.config["model"],
             messages=messages,
             **params
         )
+        return response.choices[0].message.content

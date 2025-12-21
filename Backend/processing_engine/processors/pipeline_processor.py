c77c33e940fef58fea9466321bace06569e13ba0
= <thenish2004@gmail.com>
2025-12-21 09:56:52 +0500

    Working on pipeline processor

--- Backend/processing_engine/processors/pipeline_processor.py
+++ Backend/processing_engine/processors/pipeline_processor.py
@@ -12,5 +12,28 @@ from processing_engine.processor_utils.llm_client import LLMClient
 from processing_engine.processor_utils.prompts import markdown_messages
 from processing_engine.processor_utils.prompts import json_messages
 from processing_engine.processor_utils.doc_utils import fetch_file, pdf_to_images, to_base64
-from processing_engine.models.schemas import QueueJob, ExtractedContent,Alert, AlertArea, StructuredAlert
+from processing_engine.models.schemas import QueueJob, Alert, AlertArea, StructuredAlert
 
+class PipelineProcessor():
+    def __init__(self, llm: str):
+        self.llm = LLMClient(llm)
+    
+    async def transform(self, job):
+        lmao=1
+    
+    async def _prepare_images(self, job: QueueJob) -> List[bytes]:
+        """Convert document to images (base64)""" 
+        file = await fetch_file(str(job.message.url))
+        encoded_images = []
+
+        if job.message.filetype in ["gif", "png", "jpeg", "jpg"]:
+            pil_image = Image.open(io.BytesIO(file))
+            encoded_images.append(to_base64(pil_image))
+        
+        elif job.message.filetype == "pdf":
+            pil_images = pdf_to_images(file)
+            for pil_image in pil_images:
+                encoded_images.append(to_base64(pil_image))
+        
+        return encoded_images
+        
